7177f75c082b0d3d60a85f7339cdb4b82dc0d388
Galaxy1036 <simon.hamelin6500@gmail.com>
2019-07-01 14:37:46 +0200

    2.8 update

--- Packet/PreAuth.py
+++ Packet/PreAuth.py
@@ -10,10 +10,10 @@ class PreAuth(Writer):
 
     def process(self):
         self.putInt(2)
-        self.putInt(25)
+        self.putInt(26)
         self.putInt(3)
         self.putInt(0)
-        self.putInt(1708)
+        self.putInt(1837)
         self.putString('')
         self.putInt(2)
         self.putInt(2)
